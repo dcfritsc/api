c1b819823253f6ef601c977191ec82b39af5c973
dcfritsc <danielcfritsche@gmail.com>
2020-04-22 07:58:00 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ import numpy as np
 import statistics
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
+from xml.etree import ElementTree as ET
+import requests
 
 app = Flask(__name__)
 
@@ -19,9 +21,6 @@ CORS(app)
 
 @app.route('/stateboundaries',methods=['GET'])
 def stateboundaries():
-    from xml.etree import ElementTree as ET
-import requests
-
     url = 'http://econym.org.uk/gmap/states.xml'
     page = requests.get(url)
     tree = ET.fromstring(page.content)
